f8375f2d0e364948efe58c8bb099b7cd75158a0c
IR-0 <ladabonewingl@gmail.com>
2024-04-06 12:10:00 +0300

    first наработки

---
+++ Последний урок/data/requ.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Requ(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'req'
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
+    content = sqlalchemy.Column(sqlalchemy.String)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    id_for = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    id_in = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    user = orm.relationship('User')
